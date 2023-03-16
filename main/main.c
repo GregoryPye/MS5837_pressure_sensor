9a4caa6173a7b43d6f377ede193694be78994507
Greg Pye <greg.pye@gmail.com>
2023-03-16 14:04:00 +0000

    Initial commit

---
+++ main/main.c
@@ -0,0 +1,42 @@
+
+
+/**
+ *
+ */
+
+/*********************
+ *      INCLUDES
+ *********************/
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "esp_system.h"
+#include "driver/gpio.h"
+
+#include "esp_log.h"
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ *  STATIC PROTOTYPES
+ **********************/
+
+/**********************
+ *  STATIC VARIABLES
+ **********************/
+
+static const char *TAG = "MS5837 main";
+
+void app_main(void)
+{
+    ESP_LOGI(TAG, "Main started");
+}
